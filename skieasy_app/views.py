74adf5449b1352c189889163d53123c1bb358ea9
tylowe-labs <59340807+tylowe-labs@users.noreply.github.com>
2023-04-02 16:48:04 -0400

    Update View Message For Validation

--- skieasy_app/views.py
+++ skieasy_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homePageView(request):
-    return HttpResponse("Hello Watcher!!!")
+    return HttpResponse("Hello From Django!")
