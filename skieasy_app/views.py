eb434cfeda6d57e224c54f7a7870b879904cff9d
tylowe-labs <59340807+tylowe-labs@users.noreply.github.com>
2023-04-06 20:37:07 -0400

    Add CI For Building Django Project on PRs
    Add Django build (to verify code changes build) and Python linter (pycodestyle + isort) to keep code quality on PRs into master

--- skieasy_app/views.py
+++ skieasy_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+
 def homePageView(request):
     return HttpResponse("Hello From Django!")
