5129be3172c72ca122511e994bc6228d6a5370c0
Alex Barrios <ambarrio@andrew.cmu.edu>
2023-04-11 14:23:33 -0400

    Created remaining models

--- skieasy_app/models.py
+++ skieasy_app/models.py
@@ -23,6 +23,20 @@ USER_CHOICES = (
     ('Snowboard', 'SNOWBOARD'),
 )
 
+SKILL_LEVEL = (
+    ('Beginner', 'BEGINNER'),
+    ('Intermediate', 'INTERMEDIATE'),
+    ('Advanced', 'ADVANCED'),
+    ('Expert', 'EXPERT'),
+)
+
+WEAR_STATUS = (
+    ('Factory-New', 'FACTORY-NEW'),
+    ('Minimal-Wear', 'MINIMAL-WEAR'),
+    ('Field-Tested', 'FIELD-TESTED'),
+    ('Well-Worn', 'WELL-WORN'),
+    ('Battle-Scarred', 'BATTLE-SCARRED'),
+)
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -38,3 +52,53 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'id={self.id}, user="{self.user}"'
+
+
+class Equipment(models.Model):
+    profileId = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, max_length=1000) #
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    equipmentProductName = models.CharField(max_length=1000)
+    bindingsProductName = models.CharField(max_length=1000)
+    bootsProductName = models.CharField(max_length=1000)
+    skillLevel = models.CharField(max_length=25,
+                              choices=SKILL_LEVEL, default='Beginner')
+    equipmentHeight = models.DecimalField(max_digits=10, decimal_places=2)
+    bootSize = models.DecimalField(max_digits=10, decimal_places=1)
+    wearStatus = models.CharField(max_length=25,
+                              choices=WEAR_STATUS, default='Factory-New')
+    equipmentType = models.CharField(max_length=10,
+                                choices=USER_CHOICES, default='Ski')
+    
+    def __str__(self):
+        return f'id={self.id}, profile="{self.profileId}"'
+
+
+class EquipmentImages(models.Model):
+    image = models.FileField(blank=True)
+    content_type = models.CharField(max_length=50)
+    equipmentId = models.ForeignKey(Equipment, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'id={self.id}, equipment="{self.equipmentId}"'
+    
+class EquipmentListing(models.Model):
+    equipmentId = models.ForeignKey(Equipment, on_delete=models.PROTECT)
+    profileId = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+
+    def __str__(self):
+        return f'id={self.id}, equipment="{self.equipmentId}", profile="{self.profileId}"'
+    
+
+class EquipmentReservation(models.Model):
+    equipmentId = models.ForeignKey(Equipment, on_delete=models.PROTECT)
+    profileId = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+
+    def __str__(self):
+        return f'id={self.id}, equipment="{self.equipmentId}", profile="{self.profileId}"'
+    
