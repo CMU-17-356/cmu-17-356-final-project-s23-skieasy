14712349a0845f391356d394761277d83ccc928e
Tyler Lowe <tylerlowe@gmail.com>
2023-04-18 01:50:22 -0400

    Reduce Size of Listing Cards

--- skieasy/urls.py
+++ skieasy/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from skieasy_app import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.welcome, name='welcome'),
