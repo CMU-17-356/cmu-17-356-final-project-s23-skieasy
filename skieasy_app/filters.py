488b25e3a3a4245a2018d619c1cdeea75f31119b
Tyler Lowe <tylerlowe@gmail.com>
2023-04-22 13:29:43 -0400

    Fix some style issues

--- skieasy_app/filters.py
+++ skieasy_app/filters.py
@@ -3,28 +3,66 @@ from .models import Equipment
 
 
 class EquipmentFilter(django_filters.FilterSet):
-    # pass min-max price query params to request equipment in a range of prices 
-    min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
-    max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    '''
+    Pass min-max price query params to request equipment within a 
+    range of prices.
+    Ex:
+    <url>?min_price=10&max_price=20 => equipment within [$10, $20]
+    '''
+    min_price = django_filters.NumberFilter(
+        field_name='price', lookup_expr='gte'
+    )
+    max_price = django_filters.NumberFilter(
+        field_name='price', lookup_expr='lte'
+    )
 
-    # pass min-max_boot_size query params to request equipment in a range of sizes
-    min_boot_size = django_filters.NumberFilter(field_name='boot_size', lookup_expr='gte')
-    max_boot_size = django_filters.NumberFilter(field_name='boot_size', lookup_expr='lte')
+    '''
+    Pass min-max boot_size query params to request equipment within a
+    range of boot sizes.
+    Ex:
+    <url>?min_boot_size=9&max_boot_size=10 => equipment with boots
+    within [9,10]
+    '''
+    min_boot_size = django_filters.NumberFilter(
+        field_name='boot_size', lookup_expr='gte'
+    )
+    max_boot_size = django_filters.NumberFilter(
+        field_name='boot_size', lookup_expr='lte'
+    )
 
-    # pass min-max_equipment_height query params to request equipment in a range of heights
-    min_equipment_height = django_filters.NumberFilter(field_name='equipment_height', lookup_expr='gte')
-    max_equipment_height = django_filters.NumberFilter(field_name='equipment_height', lookup_expr='lte')
+    '''
+    Pass min-max equipment_height params to request equipment within a
+    range of heights.
+    Ex:
+    <url>?min_equipment_height=70&max_equipment_height=80 => equipment
+    with a height within [70,80]
+    '''
+    min_equipment_height = django_filters.NumberFilter(
+        field_name='equipment_height', lookup_expr='gte'
+    )
+    max_equipment_height = django_filters.NumberFilter(
+        field_name='equipment_height', lookup_expr='lte'
+    )
 
-    # pass acceptable neighborhoods query params to request equipment that resides in those neighborhoods
-    # passed in like so 'neighborhoods=Downtown,Shadyside,Southside'
-    neighborhoods = django_filters.CharFilter(method='filter_neighborhood')
+    '''
+    Pass neighborhoods query params to request equipment that resides
+    within one of the neighborhoods.
+    Ex:
+    <url>?neighborhoods=Downtown,Shadyside => equipment that is located
+    within (Downtown, Shadyside)
+    '''
+    neighborhoods = django_filters.CharFilter(
+        method='filter_neighborhood'
+    )
 
-    # TODO -> add filter to find equipment provided start_date, end_date & using intervals algo
+    # TODO -> add filter to find equipment with [start_date, end_date]
+    # & using intervals algo
 
     class Meta:
         model = Equipment
         fields = [
-            'equipment_type', # default for field will match 'exact' filter
+            # default filter for a field will match 'exact'
+            'equipment_type',
             'wear_status', 
             'boot_size', 
             'equipment_height',
